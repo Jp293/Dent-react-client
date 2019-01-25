0b10c4ac40e4ac70e22ed79fe760c0846edbe2c3
Jp293 <jppr293@gmail.com>
2019-01-25 11:10:13 -0500

    Includes proper url for production api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://dent-rails-development.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
