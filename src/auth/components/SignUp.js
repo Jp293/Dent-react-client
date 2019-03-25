c589b34ccc3d9257218a2b9277d8cb1b4a8d2059
Jp293 <jppr293@gmail.com>
2019-03-24 22:34:29 -0400

    Implements styling for sign-up form. Installs packages.

--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -4,6 +4,12 @@ import Recaptcha from 'react-recaptcha'
 import { handleErrors, signUp, signIn } from '../api'
 import messages from '../messages'
 import apiUrl from '../../apiConfig'
+import TextField from '@material-ui/core/TextField'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Button from '@material-ui/core/Button'
 
 class SignUp extends Component {
   constructor () {
@@ -16,9 +22,11 @@ class SignUp extends Component {
       email: '',
       password: '',
       passwordConfirmation: '',
-      isVerified: false
+      isVerified: false,
+      showPassword: false,
     }
   }
+
 
   recaptchaLoaded = event => {
     console.log('recaptcha loaded')
@@ -42,6 +50,9 @@ class SignUp extends Component {
     [event.target.name]: event.target.value
   })
 
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword : !state.showPassword }))
+  }
   signUp = event => {
     event.preventDefault()
 
@@ -68,44 +79,77 @@ class SignUp extends Component {
     return (
       <form className='auth-form' onSubmit={this.signUp}>
         <h3>Sign Up</h3>
-
-        <label htmlFor="email">Email</label>
-        <input
+        <TextField
+          id="outlined-email-input"
+          label="Email"
           required
           name="email"
           value={email}
           type="email"
           placeholder="Email"
           onChange={this.handleChange}
+          variant="outlined"
         />
-        <label htmlFor="password">Password</label>
-        <input
+        <TextField
           required
+          id="outlined-adornment-password"
+          variant="outlined"
+          label="Password"
           name="password"
           value={password}
-          type="password"
           placeholder="Password"
           onChange={this.handleChange}
+          type={this.state.showPassword ? 'text': 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                >
+                  {this.state.showPassword ? <VisibilityOff/> : <Visibility/> }
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
         />
-        <label htmlFor="passwordConfirmation">Confirm Password</label>
-        <input
+        <TextField
           required
+          id="outlined-adornment-pconfirmation"
+          variant="outlined"
+          label="Password Confirmation"
           name="passwordConfirmation"
           value={passwordConfirmation}
-          type="password"
           placeholder="Confirm Password"
           onChange={this.handleChange}
+          type={this.state.showPassword ? 'text': 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                >
+                  {this.state.showPassword ? <VisibilityOff/> : <Visibility/> }
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
         />
+
         <Recaptcha
           sitekey="6LcCc5kUAAAAAFu_KDQKPPrcjvaIeKEbsmnNr2aw"
           render="explicit"
           onloadCallback={this.recaptchaLoaded}
           verifyCallback={this.verifyCallback}
         />
-        <button onClick={this.handleSecurity} type="submit">Sign Up</button>
+        <Button variant="outlined" color="primary" onClick={this.handleSecurity} type="submit">
+        Sign Up
+        </Button>
       </form>
     )
   }
 }
 
 export default withRouter(SignUp)
+// export withStyles(styles)(OutlineInputAdornments)
